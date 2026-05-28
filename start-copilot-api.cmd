d141ad06bf6e4ad95592282662d1a6507b9678cc
Wenbo Zhao <179686042+wenbo97@users.noreply.github.com>
2026-05-28 23:51:19 +0800

    Add cip.cc check to start-copilot-api.cmd

--- start-copilot-api.cmd
+++ start-copilot-api.cmd
@@ -5,6 +5,8 @@ echo Start Copilot API Server at %~dp0
 echo ================================================
 echo.
 
+curl cip.cc
+
 ECHO Starting Copilot-Api service...
 
 CALL CD /D %~dp0 && npm run dev
